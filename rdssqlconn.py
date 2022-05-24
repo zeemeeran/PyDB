4491771455e77cf844bc0ae971069f7aa0578fa5
Zahida Meeran <dev.zmeeran@gmail.com>
2022-05-24 15:24:09 -0400

    add code to connect to rds mysql database form local machine

---
+++ rdssqlconn.py
@@ -0,0 +1,27 @@
+import pymysql
+
+# url parts for fetching data using api
+
+def mysqlconnect():
+# To connect MySQL database
+    conn = pymysql.connect(
+        host='stock-mysqldb.cdedk051ucx3.us-east-1.rds.amazonaws.com',
+        port=3306,
+        user='admin', 
+        password = "mysqladmin",
+        db='stockmysqldb',
+        )
+    return conn
+
+conn = mysqlconnect()
+cur = conn.cursor()
+    
+# Select query
+cur.execute("select * from stocktracker")
+records = cur.fetchall()
+
+for i in records:
+    print(i)
+
+cur.close()
+conn.close()
